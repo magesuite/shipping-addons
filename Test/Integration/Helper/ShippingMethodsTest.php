bf64ba1e6f0e1e9b2ce3906f352645f1fb622437
Blazej Doleska <blazej.doleska@creativestyle.pl>
2020-05-13 12:43:47 +0200

    added integration tests

---
+++ Test/Integration/Helper/ShippingMethodsTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace MageSuite\ShippingAddons\Test\Integration\Helper;
+
+class ShippingMethodsTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var \Magento\TestFramework\ObjectManager
+     */
+    protected $objectManager;
+
+    /**
+     * @var \MageSuite\ShippingAddons\Helper\ShippingMethods
+     */
+    protected $shippingMethodsHelper;
+
+    public function setUp()
+    {
+        $this->objectManager = \Magento\TestFramework\ObjectManager::getInstance();
+
+        $this->shippingMethodsHelper = $this->objectManager->get(\MageSuite\ShippingAddons\Helper\ShippingMethods::class);
+    }
+
+    /**
+     * @magentoAppArea frontend
+     * @magentoAppIsolation enabled
+     * @magentoDbIsolation enabled
+     * @magentoConfigFixture current_store carriers/freeshipping/active 1
+     * @magentoConfigFixture current_store carriers/freeshipping/free_shipping_subtotal 40
+     * @magentoConfigFixture current_store carriers/flatrate1/active 1
+     * @magentoConfigFixture current_store carriers/flatrate1/minimum_subtotal_for_free_shipping 100
+     */
+    public function testItReturnFreeShippingMethod()
+    {
+        $freeShippingAmount = $this->shippingMethodsHelper->getMinimumFreeShippingAmount();
+
+        $this->assertEquals(40, $freeShippingAmount);
+    }
+
+    /**
+     * @magentoAppArea frontend
+     * @magentoAppIsolation enabled
+     * @magentoDbIsolation enabled
+     * @magentoConfigFixture current_store carriers/freeshipping/active 1
+     * @magentoConfigFixture current_store carriers/freeshipping/free_shipping_subtotal 100
+     * @magentoConfigFixture current_store carriers/flatrate1/active 1
+     * @magentoConfigFixture current_store carriers/flatrate1/minimum_subtotal_for_free_shipping 40
+     */
+    public function testItReturnFlatrateShippingMethod()
+    {
+        $freeShippingAmount = $this->shippingMethodsHelper->getMinimumFreeShippingAmount();
+
+        $this->assertEquals(40, $freeShippingAmount);
+    }
+
+    /**
+     * @magentoAppArea frontend
+     * @magentoAppIsolation enabled
+     * @magentoDbIsolation enabled
+     * @magentoConfigFixture current_store carriers/freeshipping/active 0
+     * @magentoConfigFixture current_store carriers/flatrate1/active 0
+     */
+    public function testItReturnNullWhenNoFreeShippingMethods()
+    {
+        $freeShippingAmount = $this->shippingMethodsHelper->getMinimumFreeShippingAmount();
+
+        $this->assertNull($freeShippingAmount);
+    }
+}
